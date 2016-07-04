d0fab622d74516fff9f0d8f20a6d50dd211916f6
iclemente <iclemente@ezconet.com.br>
2016-07-04 15:25:45 -0300

    Teste de commit

---
+++ NetCoders.SisCliente.UI.WEB/Models/ClienteMOD.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NetCoders.SisCliente.UI.WEB.Models
+{
+    [Table("TB_CLIENTE")]
+    public class ClienteMOD
+    {
+        [Key]
+        public int Codigo { get; set; }
+
+        //O Data Anottations Required, Serve pra dizer que a minha propriedade
+        //è obrigatória.
+        [Required(ErrorMessage = "Campo nome não pode ser vazio.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo e-mail não pode ser vazio."), EmailAddress]
+        //O Data Annotations EmailAddress serve para dizer que a minha
+        //Propriedade é um Email, ou seja, ele vai fazer uma verficação
+        //simples, se o campo é um email
+        public string Email { get; set; }
+    }
+}
---
+++ NetCoders.SisCliente.UI.WEB/Repositorio/Conexao.cs
@@ -0,0 +1,19 @@
+﻿using NetCoders.SisCliente.UI.WEB.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace NetCoders.SisCliente.UI.WEB.Repositorio
+{
+    public class Conexao : DbContext
+    {
+        public Conexao() : base(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=cursos;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
+        {
+
+        }
+
+        public DbSet<ClienteMOD> Cliente { get; set; }
+    }
+}
